b1dcf6b9ca40e0fb56e1f581f3556f03eb6c208a
minaazab97 <Minahannaa10@gmail.com>
2024-12-22 18:27:38 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tWwmVuuPY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'bfra0uuds1pok1a42spdcj7cn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_C5U9QHRQW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ctom8fvtggikkhfg3frtdeun9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://av8t0bj3ia.execute-api.us-east-1.amazonaws.com/UserProfile/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };

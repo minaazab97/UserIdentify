90d6a7f16de8ec16eafb40180df5a1beb0405b94
minaazab97 <Minahannaa10@gmail.com>
2024-12-22 18:47:41 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_C5U9QHRQW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3ctom8fvtggikkhfg3frtdeun9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '94f26jk8gainhpnl69f1vvkepr4gm7f8i85hms9q6rk69u4vs6g', // Ensure this is set correctly
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
